67fdb097d7eb8be119db102af971d360522ce52a
zolfaShefreie <44172962+zolfaShefreie@users.noreply.github.com>
2020-11-24 18:49:52 +0330

    add exam file serializer

--- BackExam/designexam/serializers.py
+++ BackExam/designexam/serializers.py
@@ -2,14 +2,30 @@ from rest_framework import serializers
 import datetime
 from .models import Exam
 
+class ExamFileSerializer(serializers.Serializer):
+    questions_file = serializers.FileField(write_only=True, allow_null=True)
+
+    def __init__(self, Exam_id):
+        self.instance = Exam.objects.get(id = Exam_id)
+
+    def validate_questions_file(self, value):
+        file = value
+        if file.content_type not in ['application/pdf']:
+            raise serializers.ValidationError("the format is invalid")
+        return file
+
+    def save_file(self):
+        pass
+
+    def update_instance(self):
+        pass
 
 class ExamSerializer(serializers.ModelSerializer):
-    questions_file = serializers.FileField(write_only=True, allow_null=True)
 
     class Meta:
         model = Exam
         fields = "__all__"
-        read_only_fields = ('author', 'file_URL', 'have_file')
+        read_only_fields = ('author', 'file_URL', 'have_file', 'create_date')
 
     def validate_start_date(self, value):
         if value > datetime.datetime.now():
@@ -40,11 +56,17 @@ class ExamSerializer(serializers.ModelSerializer):
         
         raise serializers.ValidationError('start_time + duration is not less than end_time')
 
-    def validate_questions_file(self, value):
-        file = value
-        if file.content_type not in ['application/pdf']:
-            raise serializers.ValidationError("the format is invalid")
-        return file
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+
+        if instance.start_time > datetime.datetime.now():
+            instance.start_time = validated_data.get('start_time', instance.start_time)
+            instance.end_time = validated_data.get('end_time', instance.end_time)
+
+        if instance.end_time > datetime.datetime.now():
+            instance.duration = validated_data.get('duration', instance.duration)
+
 
     
 
