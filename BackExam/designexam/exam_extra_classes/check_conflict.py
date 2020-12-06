61b3cf14d01d2746180f01037d7f5270ae456e48
zolfaShefreie <z.shefreie@gmail.com>
2020-12-06 23:33:58 +0330

    Create check_conflict.py

---
+++ BackExam/designexam/exam_extra_classes/check_conflict.py
@@ -0,0 +1,31 @@
+from client_process.get_classes import get_commen_classes
+from designexam.models import Exam
+
+class ExamConflictChecker:
+
+    def __init__(self, start_time, end_time, course_id):
+        self.start_time = start_time
+        self.end_time = end_time
+        self.course_id = course_id
+    
+    def get_class_list(self):
+        classes = get_commen_classes(self.course_id)
+        #check the type of classes
+        return classes
+
+    def has_conflict(self):
+        course_list = self.get_class_list
+        exams = Exam.objects.filter(courseID__in=course_list, start_date__gt=self.end_time, 
+                                    end_date__lte=self.end_time)
+        
+        if exams:
+            return True
+
+        exams = Exam.objects.filter(courseID__in=course_list, start_date__gte=self.start_time, 
+                                    end_date__lt=self.start_time)
+
+        if exams:
+            return True
+        
+        return False
+        
