e78153c5ccfabbdb216c50213693d4ddadb94ce1
zolfaShefreie <z.shefreie@gmail.com>
2020-12-06 12:46:35 +0330

    add client of get commen classes

--- BackExam/client_process/get_classes.py
+++ BackExam/client_process/get_classes.py
@@ -7,4 +7,12 @@ def get_classes(id):
         user_id = class_pb2.ClassListRequest(id)
         classes = stub.List(user_id)
         return classes
+
+def get_commen_classes(id):
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = class_pb2_grpc.ClassControllerStub(channel)
+        class_id = class_pb2.ClassCommenRequest(id)
+        classes = stub.GetCommenClasses(class_id)
+        return classes
+
         
