71b4dbfcce7f39ff5e975d0ea0457b3ea5ca5c2a
zolfaShefreie <z.shefreie@gmail.com>
2020-12-07 20:46:39 +0330

    overwrite the Update service

--- BackExam/account/services.py
+++ BackExam/account/services.py
@@ -3,6 +3,17 @@ from django_grpc_framework import generics
 from account.serializers import UserProtoSerializer
 
 
-class UserService(generics.ModelService):
+class UserService(generics.CreateService, generics.UpdateService, generics.DestroyService):
     queryset = User.objects.all()
     serializer_class = UserProtoSerializer
+
+    def Update(self, request, context):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, message=request, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+
+        return serializer.message
