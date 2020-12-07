f1bfe117a5501e7a0f5372d0a799adb79681660e
zolfaShefreie <z.shefreie@gmail.com>
2020-12-07 20:41:20 +0330

    Revert "update services.py"
    This reverts commit 4931cfd518ffbc4b8358bf218664f528c509489d.

--- BackExam/account/services.py
+++ BackExam/account/services.py
@@ -3,6 +3,6 @@ from django_grpc_framework import generics
 from account.serializers import UserProtoSerializer
 
 
-class UserService(generics.CreateService, generics.UpdateService, generics.DestroyService):
+class UserService(generics.ModelService):
     queryset = User.objects.all()
     serializer_class = UserProtoSerializer
