4931cfd518ffbc4b8358bf218664f528c509489d
zolfaShefreie <z.shefreie@gmail.com>
2020-12-07 20:24:35 +0330

    update services.py

--- BackExam/account/services.py
+++ BackExam/account/services.py
@@ -3,6 +3,6 @@ from django_grpc_framework import generics
 from account.serializers import UserProtoSerializer
 
 
-class UserService(generics.ModelService):
+class UserService(generics.CreateService, generics.UpdateService, generics.DestroyService):
     queryset = User.objects.all()
     serializer_class = UserProtoSerializer
