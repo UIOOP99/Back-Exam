89718b3516794155404e59d6db8e9bf9a103da15
zolfaShefreie <z.shefreie@gmail.com>
2020-12-08 22:32:33 +0330

    change the user model

--- BackExam/account/models.py
+++ BackExam/account/models.py
@@ -1,15 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
 
 
-class User(AbstractUser):
+class User(AbstractBaseUser):
     password = None
-    first_name = None
-    last_name = None
-    email = None
-    groups = None
-    date_joined = None
     last_login = None
     role_choices = (('PROFESSOR', 'PROFESSOR'), ('ADMIN', 'ADMIN'), ('STUDENT', 'STUDENT'))
     id = models.PositiveIntegerField(primary_key=True, null=False)
     role = models.CharField(max_length=10, null=False, choices=role_choices)
+    USERNAME_FIELD = 'id'
