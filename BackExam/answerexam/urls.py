8db2252c9e8aa370f90e6ecb0501398a9d10b02f
farinam_hz <f.hemmatizadeh@gmail.com>
2021-01-09 00:24:14 +0330

    "url modified"

--- BackExam/answerexam/urls.py
+++ BackExam/answerexam/urls.py
@@ -8,7 +8,7 @@ urlpatterns=[
     path('multiple-answer/', views.DescriptiveAnswerViewSet.as_view({
         'post': "create",
     })),
-    path('file/<int:pk>/', views.DescriptiveAnswerViewSet.as_view({
+    path('file/<int:pk>/', views.MultipleAnswerViewSet.as_view({
         "post": "create_file",
         "get": "get_file_url"})),
 ]
