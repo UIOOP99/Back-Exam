e866b20537adf324fef254ccc8d7716bc48613e3
farinam_hz <f.hemmatizadeh@gmail.com>
2021-01-08 22:11:27 +0330

    "urls for answerexam modifed"

--- BackExam/answerexam/urls.py
+++ BackExam/answerexam/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('answerexam/dscriptive_answer/', views.DescriptiveAnswerViewSet.as_view({
+    path('dscriptive_answer/', views.DescriptiveAnswerViewSet.as_view({
         'post': "create",
     })),
-    path('answerexam/file/<int:pk>/', views.DescriptiveAnswerViewSet.as_view({
+    path('file/<int:pk>/', views.DescriptiveAnswerViewSet.as_view({
         "post": "create_file",
         "get": "get_file_url"})),
 ]
