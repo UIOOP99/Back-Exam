8efc7ab3fc9a6bde9f4630a37eb0fb6e8720b604
farinam_hz <f.hemmatizadeh@gmail.com>
2021-01-08 22:14:03 +0330

    "spell modified"

--- BackExam/answerexam/urls.py
+++ BackExam/answerexam/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('dscriptive_answer/', views.DescriptiveAnswerViewSet.as_view({
+    path('descriptive_answer/', views.DescriptiveAnswerViewSet.as_view({
         'post': "create",
     })),
     path('file/<int:pk>/', views.DescriptiveAnswerViewSet.as_view({
