dd68e63ae10e97bafb347c8a0992f0306a45bfac
farinam_hz <f.hemmatizadeh@gmail.com>
2021-01-21 23:31:08 +0330

    "URLs added for creating questions with their files"

--- BackExam/designexam/urls.py
+++ BackExam/designexam/urls.py
@@ -15,4 +15,19 @@ urlpatterns=[
     # path("exams/", views.ExamViewSet.as_view({"get": "get_exams",})),
     path("exams/", views.Exams.as_view()),
     path("exams/<str:course_id>/", views.CourseExams.as_view()),
+
+
+    path('descriptive-question/', views.DescriptiveQuestionViewSet.as_view({
+        'post': "create",
+    })),
+    path('multiple-question/', views.MultipleQuestionViewSet.as_view({
+        'post': "create",
+    })),
+    path('descriptive-question/file/<int:pk>/', views.DescriptiveQuestionViewSet.as_view({
+        "post": "create_file",
+        "get": "get_file_url"})),
+
+    path('descriptive-question/file/<int:pk>/', views.MultipleQuestionViewSet.as_view({
+        "post": "create_file",
+        "get": "get_file_url"})),
 ]
