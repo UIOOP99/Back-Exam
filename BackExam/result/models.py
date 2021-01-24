280be629f87e3bbb068f51ecb36225fdac69c037
zolfaShefreie <z.shefreie@gmail.com>
2021-01-25 02:21:18 +0330

    add index and unique to model

--- BackExam/result/models.py
+++ BackExam/result/models.py
@@ -6,3 +6,9 @@ class Result(models.Model):
     mark = models.FloatField(null=False)
     types = (('Descriptive', 'Descriptive'), ('Multiple', 'Multiple'))
     question_type = models.CharField(choices=types, null=False, max_length=20)
+
+    class Meta:
+       unique_together = ('answer_id', 'question_type', )
+       index_together = [
+           ['answer_id', 'question_type', ]
+       ]
