6be191c5aee8878dae48b26296ee545953ea8218
farinam_hz <f.hemmatizadeh@gmail.com>
2021-01-24 19:17:04 +0330

    "que ids indexed"

--- BackExam/answerexam/models.py
+++ BackExam/answerexam/models.py
@@ -9,6 +9,11 @@ class MultipleAnswer(models.Model):  # Students
     answer_choice = models.CharField(max_length=10)  # string of 0 & 1 that 0 is for False and 1 is for True
     created_date = models.DateTimeField(auto_now_add=True, blank=False)
 
+    class Meta:
+        index_together = [
+            ["multiple_questionID_id"],
+        ]
+
 
 class DescriptiveAnswer(models.Model):  # Students
     descriptive_questionID = models.ForeignKey(DescriptiveQuestion, on_delete=models.CASCADE, blank=False)
@@ -16,4 +21,9 @@ class DescriptiveAnswer(models.Model):  # Students
     file_id =models.PositiveIntegerField(default=None, null=True)  # if teacher allow them
     text = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True, blank=False)
+
+    class Meta:
+        index_together = [
+            ["descriptive_questionID_id"],
+        ]
 
