287421c87373ac23ba13f0f93a16e26bab7979af
zolfaShefreie <z.shefreie@gmail.com>
2021-01-24 16:35:59 +0330

    create model result

--- BackExam/result/models.py
+++ BackExam/result/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from designexam.models import Exam
 
-# Create your models here.
+
+class Result(models.Model):
+    exam = models.ForeignKey(Exam, null=False, on_delete=models.CASCADE)
+    score = models.FloatField(null=False)
+    types = (('Descriptive', 'Descriptive'), ('Multiple', 'Multiple'))
+    question_type = models.CharField(choices=types, null=True)
+    
+
