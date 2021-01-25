1551c042c71bd686cba52fe4d047f790311f3a3a
zolfaShefreie <z.shefreie@gmail.com>
2021-01-25 11:45:40 +0330

    change the serializers

--- BackExam/answerexam/serializers.py
+++ BackExam/answerexam/serializers.py
@@ -123,12 +123,3 @@ class MultipleAnswerSerializer(serializers.ModelSerializer):
 
         return attr
 
-
-class MultipleAnswerReadSerializer(serializers.ModelSerializer):
-    #multiple_questionID = 
-    result = serializers.SerializerMethodField()
-    serializers.DjangoModelField
-    class Meta:
-        model = MultipleAnswer
-        fields = '__all__'
-
